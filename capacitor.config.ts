59cbfc13a851e0e6bdda7067a58e16a56307f3bd
Gabriel Moré Maia <gabriel_more@hotmail.com>
2024-01-29 21:58:44 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fala-comigo',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
